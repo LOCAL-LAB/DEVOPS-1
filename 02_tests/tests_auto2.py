f9e2721fa5013a947a086e4b3cf89f81b8a1ab58
devcom2020 <kwp@tlen.pl>
2024-04-13 16:59:56 +0200

    poprawione testy

--- 02_tests/tests_auto2.py
+++ 02_tests/tests_auto2.py
@@ -1,28 +1,24 @@
 import unittest
 
-class MyTestCase2(unittest.TestCase):
-    def test_one(self):
-        result = "do_something"
-    
-    def notest(self):
-        assert result == 'do_something'
-
 class MySecondTestCase2(unittest.TestCase):
     def test_one(self):
         pass
     
     def test_main(self):
-        wynik = addition(3,2)
-        assert wynik == 4
+        wynik = addition(3, 2)
+        self.assertEqual(wynik, 5)
 
 def addition(*args):
-    a1,a2 = args
-    return a1 + a2
+    return sum(args)
     
 def main():
     import sys
-    print(addition(a1,a2))
+    if len(sys.argv) > 2:
+        a1 = int(sys.argv[1])
+        a2 = int(sys.argv[2])
+        print(addition(a1, a2))
+    else:
+        print("")
 
 if __name__ == '__main__':
-    # unittest.main()
-    main()
+    unittest.main()
