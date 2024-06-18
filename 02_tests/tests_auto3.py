6f29b790b517be4e027e40457fc3d491c9c5d2d3
LOCAL-LAB <m.premous@gmail.com>
2024-06-18 15:55:48 +0200

    dodalem nowe testy 3

---
+++ 02_tests/tests_auto3.py
@@ -0,0 +1,18 @@
+import unittest
+
+class MyTestCase(unittest.TestCase):
+    def test_one(self):
+        pass
+
+    def test_two(self):
+        pass
+
+class MySecondTestCase(unittest.TestCase):
+    def test_3(self):
+        pass
+
+    def test_4(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
