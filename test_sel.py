3fcd6b37d48ea666bfca82d34ba70f0d3bd02b0e
LOCAL-LAB <m.premous@gmail.com>
2024-06-19 14:52:56 +0200

    nowy test selenium 3

--- test_sel.py
+++ test_sel.py
@@ -7,7 +7,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("http://alx.pl")
+driver.get("https://alx.pl")
 print(driver.title)
 driver.quit()
 
